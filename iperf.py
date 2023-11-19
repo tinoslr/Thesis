1447e0d34880c305e674e1a5bf987809b3fc33c6
Your Name <trex1402@gmail.com>
2023-11-19 15:37:29 +0100

    fiex taht shit

--- iperf.py
+++ iperf.py
@@ -2,8 +2,9 @@ import re
 
 bandwith_list=[]
 
-def extract_mbits_per_sec(file_path):
 
+def extract_mbits_per_sec(file_path):
+    
     with open(file_path, 'r') as file:
             lines = file.readlines()
             for i, line in enumerate(lines, start=1):
@@ -14,18 +15,17 @@ def extract_mbits_per_sec(file_path):
                     bandwith = float(match.group(1))
                     # append latency to an list
                     bandwith_list.append(bandwith)
-                    new_list = bandwith_list[:len(bandwith_list) - 2]
-    
-    
-    return print(bandwith_list)
-
+            del bandwith_list[-2:]
+            print(bandwith_list)
+    return bandwith_list
 
 
 def calculate_average(bandwith_list):
+    print(bandwith_list)
     sum = 0
     for i in bandwith_list:
         sum += i
-    Average = sum / (len(bandwith_list)-2)
+    Average = sum / (len(bandwith_list))
     return print(Average)
 
 
