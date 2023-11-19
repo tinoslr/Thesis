84824606ff7ee27793e45c366f93eb849796b1cd
Tino Schlaier <dark_hornet@PC-Tino.localdomain>
2023-11-19 14:48:44 +0100

    dont know anymore

--- iperf.py
+++ iperf.py
@@ -4,19 +4,10 @@ bandwith_list=[]
 
 def extract_mbits_per_sec(file_path):
 
-    # with open(file_path , 'r') as file:
-    #     text = file.read()
-
-    # mbits_sec_pattern =re.compile(r'(\d+\.\d+)\sMbits/sec')
-    # matches = mbits_sec_pattern.findall(text)
-    # print(matches)
-    # bandwith_list.append(matches)
-
-
     with open(file_path, 'r') as file:
             lines = file.readlines()
             for i, line in enumerate(lines, start=1):
-                # i dont know, google it. filters the latency part out of the ping.txt file
+                # i dont know, google it. filters the bandwith part out of the ping.txt file
                 match = re.search(r'(\d+\.\d+)\sMbits/sec', line)
                 if match:
                     # attach the value to the var = latency
@@ -24,7 +15,7 @@ def extract_mbits_per_sec(file_path):
                     # append latency to an list
                     bandwith_list.append(bandwith)  
     
-    print(bandwith_list)
+    
     return bandwith_list
 
 
@@ -33,7 +24,9 @@ def calculate_average(bandwith_list):
     sum = 0
     for i in bandwith_list:
         sum += i
-    print (sum)
+    Average = sum / len(bandwith_list)
+    return print(Average)
+
 
 file_path= 'iperf.txt'
 extract_mbits_per_sec(file_path)
