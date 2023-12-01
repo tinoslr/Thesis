d66648336b4f138153c01ac425ed2976c7529807
Tino Schlaier <trex1402@gmail.com>
2023-12-01 14:15:22 +0100

    added comments

--- CSA/csa_latency.sh
+++ CSA/csa_latency.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-ping www.google.com -c 100 -i 0.2 > ping.txt
+#ping device in the network
+#maybe use traceroute
+# use parallel streams with increasing mbits 
+ping 192.168.0.xxx -c 100 -i 0.2 > ping.txt
 
 python3 readout.py
