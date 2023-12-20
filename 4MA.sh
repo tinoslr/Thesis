a756c59c13a536a9f3de3b822bb8000e8a24a51a
Tino <tino.schlaier@siticom.de>
2023-12-20 14:31:10 +0100

    hello

--- 4MA.sh
+++ 4MA.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-iperf3 -s -D --logfile /home/r/thesis/M4a.txt -p 5001 
-iperf3 -s -D --logfile /home/r/thesis/M4b.txt -p 5002
-iperf3 -s -D --logfile /home/r/thesis/M4c.txt -p 5003
-iperf3 -s -D --logfile /home/r/thesis/M4d.txt -p 5004
+iperf3 -s -D --logfile /home/siticom/thesis/M4a.txt -p 5001 
+iperf3 -s -D --logfile /home/siticom/thesis/M4b.txt -p 5002
+iperf3 -s -D --logfile /home/siticom/thesis/M4c.txt -p 5003
+iperf3 -s -D --logfile /home/siticom/thesis/M4d.txt -p 5004
--- 5MA.sh
+++ 5MA.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-iperf3 -s -D --logfile /home/r/thesis/M5a.txt -p 5001
-iperf3 -s -D --logfile /home/r/thesis/M5b.txt -p 5002
-iperf3 -s -D --logfile /home/r/thesis/M5c.txt -p 5003
-iperf3 -s -D --logfile /home/r/thesis/M5d.txt -p 5004
-iperf3 -s -D --logfile /home/r/thesis/M5e.txt -p 5005
+iperf3 -s -D --logfile /home/siticom/thesis/M5a.txt -p 5001
+iperf3 -s -D --logfile /home/siticom/thesis/M5b.txt -p 5002
+iperf3 -s -D --logfile /home/siticom/thesis/M5c.txt -p 5003
+iperf3 -s -D --logfile /home/siticom/thesis/M5d.txt -p 5004
+iperf3 -s -D --logfile /home/siticom/thesis/M5e.txt -p 5005
