2880df8fc3eaee5ba91fd1161eb705ff422aa13c
Tino <tino.schlaier@siticom.de>
2023-12-20 14:18:19 +0100

    changed stuff

--- 5MA.sh
+++ 5MA.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-iperf3 -s -D --logfile /home/tino/Thesis/M5a.txt -p 5001
-iperf3 -s -D --logfile /home/tino/Thesis/M5b.txt -p 5002
-iperf3 -s -D --logfile /home/tino/Thesis/M5c.txt -p 5003
-iperf3 -s -D --logfile /home/tino/Thesis/M5d.txt -p 5004
-iperf3 -s -D --logfile /home/tino/Thesis/M5e.txt -p 5005
+iperf3 -s -D --logfile /home/r/thesis/M5a.txt -p 5001
+iperf3 -s -D --logfile /home/r/thesis/M5b.txt -p 5002
+iperf3 -s -D --logfile /home/r/thesis/M5c.txt -p 5003
+iperf3 -s -D --logfile /home/r/thesis/M5d.txt -p 5004
+iperf3 -s -D --logfile /home/r/thesis/M5e.txt -p 5005
