918c0bb11e59354311641700959331d58733ec3e
Tino <tino.schlaier@siticom.de>
2023-12-20 13:15:42 +0100

    helo

--- 5MA.sh
+++ 5MA.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-iperf3 -s -D --logfile M5a.txt -p 5001
-iperf3 -s -D --logfile M5b.txt -p 5002
-iperf3 -s -D --logfile M5c.txt -p 5003
-iperf3 -s -D --logfile M5d.txt -p 5004
-iperf3 -s -D --logfile M5e.txt -p 5005
+iperf3 -s -D --logfile /home/M5a.txt -p 5001
+iperf3 -s -D --logfile /home/M5b.txt -p 5002
+iperf3 -s -D --logfile /home/M5c.txt -p 5003
+iperf3 -s -D --logfile /home/M5d.txt -p 5004
+iperf3 -s -D --logfile /home/M5e.txt -p 5005
