764d93bc6a530fef585cb36d7f62b6b9fdcfc28c
Tino <tino.schlaier@siticom.de>
2023-12-20 13:23:50 +0100

    hello

--- 4MA.sh
+++ 4MA.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!bin/bash
 
-# Gib hier den gewünschten Pfad an, wo die Logdateien gespeichert werden sollen
-log_path="/home/tino/Thesis"
-
-iperf3 -s --logfile "${log_path}M4a.txt" -p 5001
-iperf3 -s --logfile "${log_path}M4b.txt" -p 5002
-iperf3 -s --logfile "${log_path}M4c.txt" -p 5003
-iperf3 -s --logfile "${log_path}M4d.txt" -p 5004
+iperf3 -s -D --logfile /home/tino/Thesis/M4a.txt -p 5001 
+iperf3 -s -D --logfile /home/tino/Thesis/M4b.txt -p 5002
+iperf3 -s -D --logfile /home/tino/Thesis/M4c.txt -p 5003
+iperf3 -s -D --logfile /home/tino/Thesis/M4d.txt -p 5004
