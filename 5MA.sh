d3e3d98e06a12831d759432f79bb05e9225c5f2a
Tino <tino.schlaier@siticom.de>
2024-01-17 13:08:44 +0100

    Changed the bash files for creeating pararell iperf session

--- 5MA.sh
+++ 5MA.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-iperf3 -s -D --logfile /home/siticom/thesis/M5a.txt -p 5001
-iperf3 -s -D --logfile /home/siticom/thesis/M5b.txt -p 5002
-iperf3 -s -D --logfile /home/siticom/thesis/M5c.txt -p 5003
-iperf3 -s -D --logfile /home/siticom/thesis/M5d.txt -p 5004
-iperf3 -s -D --logfile /home/siticom/thesis/M5e.txt -p 5005
+iperf3 -s -D --logfile /home/r/Test/M5a.txt -p 5001
+iperf3 -s -D --logfile /home/r/Test/M5b.txt -p 5002
+iperf3 -s -D --logfile /home/r/Test/M5c.txt -p 5003
+iperf3 -s -D --logfile /home/r/Test/M5d.txt -p 5004
+iperf3 -s -D --logfile /home/r/Test/M5e.txt -p 5005
