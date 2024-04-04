89c23bab33361e0635b523c3b19f6db80787562e
Tino Schlaier <dark_hornet@PC-Tino.localdomain>
2024-04-05 01:36:35 +0200

    Removed some uncessarceacreay sutff

--- testtest.py
+++
@@ -1,5 +0,0 @@
-with open('ping45.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        # Ersetze "Zeit" durch "time" und trenne die Zahl und "ms" mit einem Leerzeichen
-        modified_line = line.replace("Zeit", "time").replace("ms", " ms")
-        print(modified_line, end='')
